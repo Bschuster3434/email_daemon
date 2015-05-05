72c4de83ff3d4a95a21765da78bf9749108dee2c
Schuster Pi Server <brian.daniel.schuster@gmail.com>
2015-05-05 21:03:35 +0000

    Added HTML Support

--- test_email.py
+++ test_email.py
@@ -1,6 +1,7 @@
 import smtplib
 import datetime
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from random import choice
 import os
 
@@ -38,10 +39,25 @@ def return_email_message(message_file):
 	"""Creates the MIME message object based off the
 	   data in the message file. Supports html and
 	   txt formatted files."""
+
+	# Opening MIME object to support multiple file types
+	msg = MIMEMultipart('alternative')
+	
+	# Finding the message type from folder
 	message_type = message_file.split('.')[-1]
-	if message_type == 'txt':
-		with open(os.path.join(email_files, message_file), 'r') as f:
-			msg = MIMEText(f.read())
+
+	with open(os.path.join(email_files, message_file), 'r') as f:
+		email_message = f.read()
+		# If email message is text, MIMEText Plain
+		# Else If do html
+		if message_type == 'txt':
+			part = MIMEText(email_message, 'plain')
+		elif message_type == 'html':
+			part = MIMEText(email_message, 'html')
+		else:
+			print "File Type must be HTML or TXT"
+			raise
+	msg.attach(part)
 	msg['Subject'] = "File_Contents_%s" %message_file
 	msg['From'] = GMAIL_USERNAME
 	msg['To'] = recipient
@@ -73,12 +89,16 @@ def test():
 	
 	test_file = 'message_4.txt'
 	four_message = return_email_message(test_file)
-	
+
 	assert four_message['Subject'] == "File_Contents_message_4.txt"
 	assert four_message['From'] == GMAIL_USERNAME
 	assert four_message['To'] == recipient
-	assert four_message.as_string().split('\n')[-2]
+	assert four_message.as_string().split('\n')[-3]
 	
+	test_html = 'test.html'
+	test_message = return_email_message(test_html)
+	assert test_message['Subject'] == "File_Contents_test.html"
+	send_message(test_message)
 	print "All Tests Passed"
 
 def send_file():
@@ -89,4 +109,4 @@ def send_file():
 
 if __name__ == "__main__":
 	test()
-	send_file()
+	#send_file()
